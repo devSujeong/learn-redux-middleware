3c40b4fb34d6d98b1d80fcd5721c22e8f1d34549
Crystal <dev.sujeong@gmail.com>
2021-03-05 07:50:27 +0900

    posts 리듀서 연결

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import CounterContainer from "./containers/CounterContainer";
+import PostListContainer from "./containers/PostListContainer";
 
 function App() {
   return (
-    <CounterContainer />
+    <PostListContainer />
   );
 }
 
