e7519ade2a9b8a3099f837a28561014f188e6ba6
Crystal <dev.sujeong@gmail.com>
2021-03-06 00:04:41 +0900

    ux 개선

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -12,16 +12,58 @@ const GET_POST_FAILURE = 'post/GET_POST_FAILURE';
 const CLEAR_POST = 'post/CLEAR_POST';
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const getPost = id => async (dispatch) => {
+  dispatch({ type: GET_POST, meta: id });
+
+  try {
+    const payload = await postsAPI.getPostById(id);
+
+    dispatch({ type: GET_POST_SUCCESS, payload, meta: id});
+  } catch(e) {
+    dispatch({ type: GET_POST_FAILURE, payload: e, meta: id, error: true });
+  }
+}
 export const clearPost = () => ({ type: CLEAR_POST });
 
 const initialState = {
   posts: reducerUtils.initial(),
-  post: reducerUtils.initial()
+  post: {}
 };
 
 const getPostsReducer = handleAsyncAcitons(GET_POSTS, 'posts', true);
-const getPostReducer = handleAsyncAcitons(GET_POST, 'post');
+const getPostReducer = (state, action) => {
+  const id = action.meta;
+
+  switch(action.type) {
+    case GET_POST:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.loading(state.post[id] && state.post[id].data)
+        }
+      }
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.success(action.payload)
+        }
+      }
+    case GET_POST_FAILURE:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.error(action.payload)
+        }
+      }
+    default:
+      return state
+  }
+};
+
 export default function posts(state = initialState, action) {
   switch(action.type) {
     case GET_POSTS:
