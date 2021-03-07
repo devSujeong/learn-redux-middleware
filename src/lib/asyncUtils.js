d2f4e31f5e6d1fbf60ba3f9b21da65a1471438f7
Crystal <dev.sujeong@gmail.com>
2021-03-07 21:30:19 +0900

    redux-thunk end

--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -20,6 +20,33 @@ export const createPromiseThunk = (type, promiseCreator) => {
   };
 };
 
+const defaultIdSelector = param => param;
+
+export const createPromiseThunkById = (type, promiseCreator, idSelector = defaultIdSelector) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  return param => async dispatch => {
+    const id = idSelector(param);
+    dispatch({ type, meta: id });
+
+    try {
+      const payload = await promiseCreator(param);
+      dispatch({
+        type: SUCCESS,
+        payload,
+        meta: id
+      });
+    } catch(e) {
+      dispatch({
+        type: ERROR,
+        payload: e,
+        error: true,
+        meta: id
+      });
+    }
+  };
+};
+
 export const handleAsyncAcitons = (type, key, keepData) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
 
@@ -39,6 +66,43 @@ export const handleAsyncAcitons = (type, key, keepData) => {
         return {
           ...state,
           [key]: reducerUtils.error(action.payload),
+        }
+      default: 
+        return state;
+    }
+  }
+};
+
+export const handleAsyncAcitonsById = (type, key, keepData) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  return (state, action) => {
+    const id = action.meta;
+
+    switch(action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: {
+            ...state[key],
+            [id]: reducerUtils.loading(keepData ? (state[key][id] && state[key][id].data) : null)
+          },
+        }
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: {
+            ...state[key],
+            [id]: reducerUtils.success(action.payload),
+          } 
+        }
+      case ERROR:
+        return {
+          ...state,
+          [key]: {
+            ...state[key],
+            [id]: reducerUtils.error(action.payload),
+          }
         }
       default: 
         return state;
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,5 +1,5 @@
 import * as postsAPI from '../api/posts';
-import { createPromiseThunk, handleAsyncAcitons, reducerUtils } from '../lib/asyncUtils';
+import { createPromiseThunk, createPromiseThunkById, handleAsyncAcitons, handleAsyncAcitonsById, reducerUtils } from '../lib/asyncUtils';
 
 const GET_POSTS = 'post/GET_POSTS';
 const GET_POSTS_SUCCESS = 'post/GET_POSTS_SUCCESS';
@@ -12,18 +12,11 @@ const GET_POST_FAILURE = 'post/GET_POST_FAILURE';
 const CLEAR_POST = 'post/CLEAR_POST';
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = id => async (dispatch) => {
-  dispatch({ type: GET_POST, meta: id });
-
-  try {
-    const payload = await postsAPI.getPostById(id);
-
-    dispatch({ type: GET_POST_SUCCESS, payload, meta: id});
-  } catch(e) {
-    dispatch({ type: GET_POST_FAILURE, payload: e, meta: id, error: true });
-  }
-}
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
 export const clearPost = () => ({ type: CLEAR_POST });
+export const goToHome = () => (dispatch, getState, {history}) => {
+  history.push('/');
+};
 
 const initialState = {
   posts: reducerUtils.initial(),
@@ -31,38 +24,7 @@ const initialState = {
 };
 
 const getPostsReducer = handleAsyncAcitons(GET_POSTS, 'posts', true);
-const getPostReducer = (state, action) => {
-  const id = action.meta;
-
-  switch(action.type) {
-    case GET_POST:
-      return {
-        ...state,
-        post: {
-          ...state.post,
-          [id]: reducerUtils.loading(state.post[id] && state.post[id].data)
-        }
-      }
-    case GET_POST_SUCCESS:
-      return {
-        ...state,
-        post: {
-          ...state.post,
-          [id]: reducerUtils.success(action.payload)
-        }
-      }
-    case GET_POST_FAILURE:
-      return {
-        ...state,
-        post: {
-          ...state.post,
-          [id]: reducerUtils.error(action.payload)
-        }
-      }
-    default:
-      return state
-  }
-};
+const getPostReducer = handleAsyncAcitonsById(GET_POST, 'post', true);
 
 export default function posts(state = initialState, action) {
   switch(action.type) {
