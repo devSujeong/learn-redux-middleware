e27252d84d0f54a1f262d3821a76b7abd111ce95
Crystal <dev.sujeong@gmail.com>
2021-03-07 22:07:26 +0900

    api 변경

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,34 +1,11 @@
-const sleep = n => new Promise((resolve) => setTimeout(resolve, n));
-
-const posts = [
-  {
-    id: 1,
-    title: 'sjsjsjsjs',
-    body: 'a;a;a;a;a;a;aa;a;a;a;'
-  },
-  {
-    id: 2,
-    title: 'example',
-    body: 'redux thunk'
-  },
-  {
-    id: 3,
-    title: 'redux saga써보고 싶은데',
-    body: '잘 이해할 수 있을까'
-  },
-  {
-    id: 4,
-    title: '재미있는 놀이',
-    body: '벨로퍼트 짱'
-  },
-];
+import axios from 'axios';
 
 export const getPosts = async () => {
-  await sleep(500);
-  return posts;
+  const response = await axios.get('/posts');
+  return response.data;
 }
 
 export const getPostById = async (id) => {
-  await sleep(500);
-  return posts.find(post => post.id === id);
+  const response = await axios.get(`/posts/${id}`);
+  return response.data;
 }
