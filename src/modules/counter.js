0d7b91c7ca5818b0f9883a7ebf617dcd8826233d
Crystal <dev.sujeong@gmail.com>
2021-03-08 15:36:58 +0900

    리덕스 사가 적용

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -7,8 +7,8 @@ const DECREASE_ASYNC = 'counter/DECREASE_ASYNC';
 
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
-export const increaseAsync = () => ({ type: INCREASE_ASYNC});
-export const decreaseAsync = () => ({ type: DECREASE_ASYNC});
+export const increaseAsync = () => ({ type: INCREASE_ASYNC });
+export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
 
 function* increaseSaga() {
   yield delay(1000); // 1초 기다림
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,5 +1,6 @@
 import * as postsAPI from '../api/posts';
-import { createPromiseThunk, createPromiseThunkById, handleAsyncAcitons, handleAsyncAcitonsById, reducerUtils } from '../lib/asyncUtils';
+import { handleAsyncAcitons, handleAsyncAcitonsById, reducerUtils, createPromiseSaga, createPromiseSagaById } from '../lib/asyncUtils';
+import { takeEvery, getContext, select } from "redux-saga/effects";
 
 const GET_POSTS = 'post/GET_POSTS';
 const GET_POSTS_SUCCESS = 'post/GET_POSTS_SUCCESS';
@@ -9,14 +10,48 @@ const GET_POST = 'post/GET_POST';
 const GET_POST_SUCCESS = 'post/GET_POST_SUCCESS';
 const GET_POST_FAILURE = 'post/GET_POST_FAILURE';
 
+const GO_TO_HOME = 'post/GO_TO_HOME';
 const CLEAR_POST = 'post/CLEAR_POST';
+const PRINT_STATE = 'post/PRINT_STATE';
 
-export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
+// thunk
+// export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+// export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
+
 export const clearPost = () => ({ type: CLEAR_POST });
-export const goToHome = () => (dispatch, getState, {history}) => {
+
+export const getPosts = () => ({ type: GET_POSTS });
+export const getPost = (id) => ({
+  type: GET_POST,
+  payload: id,
+  meta: id,
+});
+export const goToHome = () => ({ type: GO_TO_HOME });
+export const printState = () => ({ type: PRINT_STATE });
+
+
+const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
+const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById);
+function* goToHomeSaga() {
+  const history = yield getContext('history');
   history.push('/');
-};
+}
+function* printStateSaga() {
+  const state = yield select(state => state.posts);
+  console.log(state);
+}
+
+export function* postsSaga() {
+  yield takeEvery(GET_POSTS, getPostsSaga);
+  yield takeEvery(GET_POST, getPostSaga);
+  yield takeEvery(GO_TO_HOME, goToHomeSaga);
+  yield takeEvery(PRINT_STATE, printStateSaga);
+}
+
+// thunk
+// export const goToHome = () => (dispatch, getState, {history}) => {
+//   history.push('/');
+// };
 
 const initialState = {
   posts: reducerUtils.initial(),
